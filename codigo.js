b959f0f3a86f1b4d27155421396926e234a878f3
MelyIriarte <iriate.mela@gmail.com>
2022-02-14 16:43:32 -0300

    simulador interactivo

--- codigo.js
+++ codigo.js
@@ -1,58 +1,54 @@
-let nombreProducto1 = "Brownie";
-let precioProducto1 = 2000;
-let stockProducto1 = 10;
+let brownie = "Brownie";
+let browniePrecio = 2000;
+let brownieStock = 10;
 
-let nombreProducto2 = "Cheescake";
-let precioProducto2 = 3000;
-let stockProducto2 = 5;
+let cheescake = "Cheescake";
+let cheescakePrecio = 3000;
+let cheescakeStock = 5;
 
-let nombreProducto3 = "Rogel";
-let precioProducto3 = 2200;
-let stockProducto3 = 3;
+let rogel = "Rogel";
+let rogelPrecio = 2200;
+let rogelStock = 3;
 
-alert("Nuestras tortas son:" + "\n" + nombreProducto1 + "\n" + nombreProducto2 + "\n" + nombreProducto3)
+let lemonpie = "Lemonpie";
+let lemonpiePrecio = 1500;
+let lemonpieStock = 6;
 
+alert("Nuestras tortas son:" + "\n" + brownie + "\n" + cheescake + "\n" + rogel + "\n" + lemonpie);
 
 let cantidadComprada;
 let total = 0;
 
-let cantidadProductos = parseInt(prompt("Ingrse la cantidad de productos a COMPRAR"));
+function stockInsuficiente(stock) {
+    alert("no tenemos suficiente stock de " + stock);
+}
 
-for (i = 0; i < cantidadProductos; i++) {
+function compra(stock, precio, producto) {
+    cantidadComprada = parseInt(prompt("Cuantos quiere:"));
+
+    if (stock >= cantidadComprada) {
+        stock = stock - cantidadComprada;
+        total += precio * cantidadComprada;
+        console.log(producto + "\nSTOCK: " + stock);
+    } else {
+        stockInsuficiente(stock);
+    }
+}
+
+let cantidadProductos = parseInt(prompt("Ingrese la cantidad de productos a COMPRAR"));
+
+for (let i = 0; i < cantidadProductos; i++) {
 
     let productoAcomprar = prompt("¿Que producto quiere comprar?");
 
-
-    if ((productoAcomprar == nombreProducto1) || (productoAcomprar == "brownie")) {
-        cantidadComprada = parseInt(prompt("ingrese la cantidad a comprar:"));
-
-        if (stockProducto1 >= cantidadComprada) {
-            stockProducto1 = stockProducto1 - cantidadComprada;
-            total += precioProducto1 * cantidadComprada;
-            console.log(nombreProducto1 + "\nSTOCK: " + stockProducto1);
-        } else { alert("no tenemos suficiente stock de " + nombreProducto1); }
-
-
-
-    } else
-    if ((productoAcomprar == nombreProducto2) || (productoAcomprar == "cheescake")) {
-        cantidadComprada = parseInt(prompt("¿Cuantas unidades desea comprar?:"));
-
-        if (stockProducto2 >= cantidadComprada) {
-            stockProducto2 = stockProducto2 - cantidadComprada;
-            total += precioProducto2 * cantidadComprada;
-            console.log(nombreProducto2 + "\nSTOCK: " + stockProducto2);
-        } else { alert("no tenemos suficiente stock."); }
-
-    } else if ((productoAcomprar == nombreProducto3) || (productoAcomprar == "rogel")) {
-        cantidadComprada = parseInt(prompt("ingrese la cantidad a comprar:"));
-
-        if (stockProducto3 >= cantidadComprada) {
-            stockProducto3 = stockProducto3 - cantidadComprada;
-            total += precioProducto3 * cantidadComprada;
-            console.log(nombreProducto3 + "\nSTOCK: " + stockProducto3);
-        } else { alert("no tenemos suficiente stock."); }
-
+    if ((productoAcomprar == brownie) || (productoAcomprar == "brownie")) {
+        compra(brownieStock, browniePrecio, brownie);
+    } else if ((productoAcomprar == cheescake) || (productoAcomprar == "cheescake")) {
+        compra(cheescakeStock, cheescakePrecio, cheescake);
+    } else if ((productoAcomprar == rogel) || (productoAcomprar == "rogel")) {
+        compra(rogelStock, rogelPrecio, rogel);
+    } else if ((productoAcomprar == lemonpie) || (productoAcomprar == "lemonpie")) {
+        compra(lemonpieStock, lemonpiePrecio, lemonpie);
     } else {
         alert("No tenemos ese producto");
     }
